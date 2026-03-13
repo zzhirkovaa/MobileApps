257d8084f7ac22d493bdebfacf9e0c73690fb34d
wntr <162570251+zzhirkovaa@users.noreply.github.com>
2026-03-13 18:22:01 +0300

    практика 1

---
+++ 1/buttonclicker/src/main/java/com/mirea/zhirkovaei/buttonclicker/MainActivity.java
@@ -0,0 +1,52 @@
+package com.mirea.zhirkovaei.buttonclicker;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tvOut;
+    private Button btnWhoAmI;
+    private CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        tvOut = findViewById(R.id.tvOut);
+        btnWhoAmI = findViewById(R.id.btnWhoAmI);
+        checkBox = findViewById(R.id.checkBox);
+
+        btnWhoAmI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tvOut.setText("Мой номер по списку № Х (по журналу)");
+                checkBox.setChecked(!checkBox.isChecked());
+            }
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void onMyButtonClick(View view) {
+        tvOut.setText("Это не я сделал");
+        Toast.makeText(this, "Ещё один способ!", Toast.LENGTH_SHORT).show();
+        checkBox.setChecked(!checkBox.isChecked());
+    }
+}
