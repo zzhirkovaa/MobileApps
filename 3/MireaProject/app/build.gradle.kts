2d3dc52b055c05b469bfa504e68ba3a262ac1027
wntr <162570251+zzhirkovaa@users.noreply.github.com>
2026-04-01 17:52:35 +0300

    практика 3 (mireaproject)

---
+++ 3/MireaProject/app/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "ru.mirea.zhirkova.mireaproject"
+    compileSdk {
+        version = release(36) {
+            minorApiLevel = 1
+        }
+    }
+
+    defaultConfig {
+        applicationId = "ru.mirea.zhirkova.mireaproject"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+dependencies {
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.appcompat:appcompat:...")
+    implementation("androidx.constraintlayout:constraintlayout:...")
+}
